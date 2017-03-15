8dcb43854cd60072414af92a0ec7b68301c42e12
leejianfeng <lee_jianfeng@sjtu.edu.cn>
2017-03-15 14:22:08 +0800

    fix show.installed

--- R/info.R
+++ R/info.R
@@ -14,8 +14,8 @@
 #' set.biosoftwares.db(sprintf('%s/.BioInstaller', tempdir()))
 #' change.info(name = 'demo', installed = 'yes', source.dir = '',
 #' bin.dir = '', excutable.files = c('demo'), others.customer = 'demo')
-change.info <- function(name = "", installed = TRUE, source.dir = "", bin.dir = "", 
-  executable.files = "", db = Sys.getenv("BIO_SOFTWARES_DB_ACTIVE", system.file("extdata", 
+change.info <- function(name = "", installed = TRUE, source.dir = "", bin.dir = "",
+  executable.files = "", db = Sys.getenv("BIO_SOFTWARES_DB_ACTIVE", system.file("extdata",
     "softwares_db_demo.yaml", package = "BioInstaller")), ..., verbose = FALSE) {
   msg <- sprintf("Running change.info for %s and be saved to %s", name, db)
   flog.info(msg)
@@ -31,7 +31,7 @@ change.info <- function(name = "", installed = TRUE, source.dir = "", bin.dir = 
   }
   Sys.setenv(R_CONFIGFILE_ACTIVE = config.cfg)
   config <- configr::read.config()
-  config[[name]] = list(installed = installed, source.dir = source.dir, bin_dir = bin.dir, 
+  config[[name]] = list(installed = installed, source.dir = source.dir, bin_dir = bin.dir,
     executable_files = executable.files, ...)
   configr::write.config(config.dat = config, write.type = "yaml")
 }
@@ -46,7 +46,7 @@ change.info <- function(name = "", installed = TRUE, source.dir = "", bin.dir = 
 #' @examples
 #' set.biosoftwares.db(sprintf('%s/.BioInstaller', tempdir()))
 #' get.info('bwa')
-get.info <- function(name = "", db = Sys.getenv("BIO_SOFTWARES_DB_ACTIVE", system.file("extdata", 
+get.info <- function(name = "", db = Sys.getenv("BIO_SOFTWARES_DB_ACTIVE", system.file("extdata",
   "softwares_db_demo.yaml", package = "BioInstaller")), verbose = FALSE) {
   db <- normalizePath(db, mustWork = FALSE)
   if (!db.check(db)) {
@@ -76,7 +76,7 @@ get.info <- function(name = "", db = Sys.getenv("BIO_SOFTWARES_DB_ACTIVE", syste
 #' @examples
 #' set.biosoftwares.db(sprintf('%s/.BioInstaller', tempdir()))
 #' del.info('bwa')
-del.info <- function(name = "", db = Sys.getenv("BIO_SOFTWARES_DB_ACTIVE", system.file("extdata", 
+del.info <- function(name = "", db = Sys.getenv("BIO_SOFTWARES_DB_ACTIVE", system.file("extdata",
   "softwares_db_demo.yaml", package = "BioInstaller")), verbose = FALSE) {
   db <- normalizePath(db, mustWork = FALSE)
   if (!db.check(db)) {
@@ -106,8 +106,8 @@ del.info <- function(name = "", db = Sys.getenv("BIO_SOFTWARES_DB_ACTIVE", syste
 #' @examples
 #' set.biosoftwares.db(sprintf('%s/.BioInstaller', tempdir()))
 #' show.installed()
-show.installed <- function(db = Sys.getenv("BIO_SOFTWARES_DB_ACTIVE", system.file("extdata", 
-  "softwares_db_demo.yaml", package = "BioInstaller")), only.installed = TRUE, 
+show.installed <- function(db = Sys.getenv("BIO_SOFTWARES_DB_ACTIVE", system.file("extdata",
+  "softwares_db_demo.yaml", package = "BioInstaller")), only.installed = TRUE,
   verbose = FALSE) {
   db <- normalizePath(db, mustWork = FALSE)
   if (!db.check(db)) {
@@ -119,11 +119,12 @@ show.installed <- function(db = Sys.getenv("BIO_SOFTWARES_DB_ACTIVE", system.fil
   }
   softwares <- c()
   Sys.setenv(R_CONFIGFILE_ACTIVE = config.cfg)
-  for (i in configr::eval.config.groups()) {
+  groups <- configr::eval.config.groups()
+  for (i in groups) {
     Sys.setenv(R_CONFIG_ACTIVE = i)
     config <- eval.config()
     if ("installed" %in% names(config)) {
-      is.installed <- config["installed"] == TRUE
+      is.installed <- config[["installed"]] %in% c(TRUE, "yes", 1)
     } else {
       is.installed <- FALSE
     }
