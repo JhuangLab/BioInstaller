2907ba101784559e9168e1bf776d7d63e297db34
leejianfeng <lee_jianfeng@sjtu.edu.cn>
2017-03-15 16:22:39 +0800

    fix mis-spell

--- R/BioInstaller.R
+++ R/BioInstaller.R
@@ -1,22 +1,22 @@
-#' Install and download massive bioinformatics analysis software and database, 
-#' such as NGS analysis tools with its required database or/and refference, 
-#' is still a complicated task. BioInstaller can be used to install 
+#' Install and download massive bioinformatics analysis software and database,
+#' such as NGS analysis tools with its required database or/and reference genome,
+#' is still a complicated task. BioInstaller can be used to install
 #' that tools and database in R conveniently.
-#' 
+#'
 #' @author
 #' Li Jianfeng \url{lee_jianfeng@sjtu.edu.cn}
 #' @seealso
 #' Useful links:
 #'
-#' \url{https://github.com/Miachol/BioInstaller} 
+#' \url{https://github.com/Miachol/BioInstaller}
 #'
-#' Report bugs at \url{https://github.com/Miachol/BioInstaller/issues} 
+#' Report bugs at \url{https://github.com/Miachol/BioInstaller/issues}
 #'
 #' @docType package
 #' @name BioInstaller
 #' @import configr stringr futile.logger
 #' @importFrom git2r clone checkout
-#' @importFrom RCurl getURL basicTextGatherer 
+#' @importFrom RCurl getURL basicTextGatherer
 #' @importFrom R.utils gunzip gzip
 #' @importFrom utils unzip untar download.file
 NULL
